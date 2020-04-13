758b3fca1c539e84902db27ee5d1bdc90473138f
thunderjet12 <kingthunder797@gmail.com>
2020-04-13 09:18:42 +0200

    Add files via upload

---
+++ app/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Import pages
+import Home from "./pages/home";
+import About from "./pages/about";
+
+const App = () => {
+  return (
+    <Router>
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
---
+++ app/components/hero.js
@@ -0,0 +1,13 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+
+function Hero() {
+    return (
+        <div>
+            <h1>GamePlay</h1>
+            <p> Welcome </p>
+        </div>
+    );
+}
+
+export default Hero
